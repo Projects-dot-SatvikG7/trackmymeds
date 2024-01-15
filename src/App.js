d7c195ddeb36f669994a738dc2deb2e9fd409d6f
Projects-dot-SatvikG7 <96785627+Projects-dot-SatvikG7@users.noreply.github.com>
2024-01-15 16:27:38 +0000

    Partial changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// Import everything needed to use the `useQuery` hook
 import { useQuery, gql } from '@apollo/client';
 import "./App.css"
-const GET_LOCATIONS = gql`
+import { useState } from 'react';
+const GET_RECORDS = gql`
   query Records {
     queryRecord {
       id
@@ -10,8 +10,46 @@ const GET_LOCATIONS = gql`
     }
   }
 `;
+
+function AddData() {
+
+  const [data, setData] = useState([
+    { "itching": false },
+    { "watery_eyes": false },
+    { "runny_nose": false },
+    { "sneezing": false },
+    { "nasal_inflammation": false },
+    { "swollen_throat": false }
+  ])
+
+  const toggle = (idx, key) => {
+    let newData = data;
+    newData[idx][key] = newData[idx][key] ? false : true;
+    setData(newData);
+    console.log(data)
+  }
+
+  if (data) return (
+    <div>
+      <div>
+        {
+          data.map((d, idx) => {
+            const key = Object.keys(d);
+            return (
+              <button key={idx} className={String(d[key])} onClick={() => toggle(idx, key)}>
+                {key}:
+              </button>
+            )
+          })
+        }
+      </div>
+      <button>Add Data Point</button>
+    </div>
+  )
+}
+
 function DisplayRecords() {
-  const { loading, error, data } = useQuery(GET_LOCATIONS);
+  const { loading, error, data } = useQuery(GET_RECORDS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
@@ -19,32 +57,38 @@ function DisplayRecords() {
   return (
     <div>
       <table id='records'>
-        <tr>
-          <th>ID</th>
-          <th>Date</th>
-          <th>Symptoms</th>
-        </tr>
-        {data.queryRecord.map((data) => (
+        <thead>
           <tr>
-            <td>{data.id}</td>
-            <td>{data.date}</td>
-            <td><ul>{
-              data.symptom.map(s => (
-                <li>
-                  {s}
-                </li>
-              ))}</ul></td>
+            <th>ID</th>
+            <th>Date</th>
+            <th>Symptoms</th>
           </tr>
-        ))}
-      </table>
+        </thead>
+        <tbody>
+          {data.queryRecord.map((data) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.date}</td>
+              <td><ul>{
+                data.symptom.map(s => (
+                  <li key={s}>
+                    {s}
+                  </li>
+                ))}</ul></td>
+            </tr>
+          ))}
+        </tbody></table>
 
     </div>
   );
 }
+
 export default function App() {
   return (
     <div>
       <h1>TrackMyMeds</h1>
+      <br />
+      <AddData />
       <br />
       <DisplayRecords />
     </div>
