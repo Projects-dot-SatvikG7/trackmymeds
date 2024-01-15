dc7b9b13c85d86f7361a93d2f59ba07206c09b4b
Projects-dot-SatvikG7 <96785627+Projects-dot-SatvikG7@users.noreply.github.com>
2024-01-16 00:50:38 +0530

    v1.0.0 released

--- src/App.js
+++ src/App.js
@@ -1,96 +1,117 @@
-import { useQuery, gql } from '@apollo/client';
-import "./App.css"
-import { useState } from 'react';
+import { useQuery, gql, useMutation } from "@apollo/client";
+import "./App.css";
+import { useState } from "react";
 const GET_RECORDS = gql`
-  query Records {
-    queryRecord {
-      id
-      date
-      symptom
+    query Records {
+        queryRecord {
+            id
+            date
+            symptom
+        }
     }
-  }
+`;
+
+const ADD_RECORD = gql`
+    mutation MyMutation($date: String!, $symptoms: [Symptom!]!) {
+        addRecord(input: { date: $date, symptom: $symptoms }) {
+            record {
+                id
+                date
+                symptom
+            }
+        }
+    }
 `;
 
 function AddData() {
-
-  const [data, setData] = useState([
-    { "itching": false },
-    { "watery_eyes": false },
-    { "runny_nose": false },
-    { "sneezing": false },
-    { "nasal_inflammation": false },
-    { "swollen_throat": false }
-  ])
-
-  const toggle = (idx, key) => {
-    let newData = data;
-    newData[idx][key] = newData[idx][key] ? false : true;
-    setData(newData);
-    console.log(data)
-  }
-
-  if (data) return (
-    <div>
-      <div>
-        {
-          data.map((d, idx) => {
-            const key = Object.keys(d);
-            return (
-              <button key={idx} className={String(d[key])} onClick={() => toggle(idx, key)}>
-                {key}:
-              </button>
-            )
-          })
+    const [addDataFunction, { data, loading, error }] = useMutation(ADD_RECORD);
+    const [props, setProps] = useState([
+        ["itching", 0],
+        ["watery_eyes", 0],
+        ["runny_nose", 0],
+        ["sneezing", 0],
+        ["nasal_inflammation", 0],
+        ["swollen_throat", 0],
+    ]);
+    const toggle = (idx) => {
+        let temp = [...props];
+        temp[idx][1] = 1 - temp[idx][1];
+        setProps(temp);
+    };
+    const addRecord = () => {
+        let date = new Date().toLocaleString();
+        let symptoms = props.filter((prop) => prop[1]).map((prop) => prop[0]);
+        console.log(date, symptoms);
+        addDataFunction({ variables: { date, symptoms } });
+        if (error) console.log(error);
+        if (data) {
+            console.log(data);
         }
-      </div>
-      <button>Add Data Point</button>
-    </div>
-  )
+    };
+    return (
+        <div className="form-flex">
+            <h2>Select Symptoms</h2>
+            <div className="btn-flex">
+                {props.map((prop, idx) => (
+                    <button
+                        key={idx}
+                        className={"btn" + (prop[1] ? " selected" : "")}
+                        onClick={() => toggle(idx)}
+                    >
+                        {prop[0].replaceAll("_", " ")}
+                    </button>
+                ))}
+            </div>
+            <button onClick={() => addRecord()} className="btn btn-process">
+                {loading ? "Recording..." : "Record Dose"}
+            </button>
+            {error && <p className="error">{error.message}</p>}
+        </div>
+    );
 }
 
 function DisplayRecords() {
-  const { loading, error, data } = useQuery(GET_RECORDS);
+    const { loading, error, data } = useQuery(GET_RECORDS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
 
-  return (
-    <div>
-      <table id='records'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Date</th>
-            <th>Symptoms</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.queryRecord.map((data) => (
-            <tr key={data.id}>
-              <td>{data.id}</td>
-              <td>{data.date}</td>
-              <td><ul>{
-                data.symptom.map(s => (
-                  <li key={s}>
-                    {s}
-                  </li>
-                ))}</ul></td>
-            </tr>
-          ))}
-        </tbody></table>
-
-    </div>
-  );
+    return (
+        <div>
+            <table id="records">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Date</th>
+                        <th>Symptoms</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.queryRecord.map((data) => (
+                        <tr key={data.id}>
+                            <td>{data.id}</td>
+                            <td>{data.date}</td>
+                            <td>
+                                <ul>
+                                    {data.symptom.map((s) => (
+                                        <li key={s}>{s.replaceAll("_", " ")}</li>
+                                    ))}
+                                </ul>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default function App() {
-  return (
-    <div>
-      <h1>TrackMyMeds</h1>
-      <br />
-      <AddData />
-      <br />
-      <DisplayRecords />
-    </div>
-  );
+    return (
+        <div>
+            <h1>TrackMyMeds</h1>
+            <AddData />
+            <DisplayRecords />
+        </div>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import App from './App';
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 const client = new ApolloClient({
-  uri: 'https://green-feather-41331517.ap-south-1.aws.cloud.dgraph.io/graphql',
-  cache: new InMemoryCache(),
+    uri: "https://green-feather-41331517.ap-south-1.aws.cloud.dgraph.io/graphql",
+    cache: new InMemoryCache(),
 });
 
 // Supported in React 18+
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
 );
+reportWebVitals();
