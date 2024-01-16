715d2ed8f694128fdcce932d35a67e62aad317cf
Projects-dot-SatvikG7 <96785627+Projects-dot-SatvikG7@users.noreply.github.com>
2024-01-16 14:14:04 +0000

    Feature: Made it PWA

---
+++ public/service-worker.js
@@ -0,0 +1,76 @@
+// *** This is cache name, change this whenever you make changes to the app ***
+const CACHE_NAME = "cache-v0";
+
+// *** This is cache name, change this whenever you make changes to the app ***
+const DYNAMIC_CACHE_NAME = "dynamic-cache-v0";
+
+// *** Array of files that you want browser to cache (Mostly static assets) and think then as fetch requested ***
+const CACHE_THIS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/static/styles/style.css",
+  "/static/js/main.js",
+  "static/assets/icons/icon-192x192.png",
+];
+
+// *** This event is fired when a new Service worker is installed ***
+self.addEventListener("install", (event) => {
+  // console.log("Service worker has been installed", event);
+  // *** Wait until browser caches the files in CACHE_THIS array declared above ***
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll(CACHE_THIS);
+    })
+  );
+});
+
+// *** This event is only fired when a new Service worker or old Service worker is activated ***
+self.addEventListener("activate", (event) => {
+  // console.log("Service worker has been activated", event);
+  // *** Wait until browser deletes old files in CACHE_THIS array declared above, only the files files corresponding to CACHE_NAME cache are saved in cache ***
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      // @ keys are basically cache names
+      // console.log(keys);
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME && key !== DYNAMIC_CACHE_NAME)
+          .map((key) => {
+            caches.delete(key);
+          })
+      );
+    })
+  );
+});
+
+/*
+@ This event intercepts every fetch request !
+
+-- If the request exists in cache it return the caches file 
+-- else if internet connection is available then the browser continues the request and the response is stored in DYNAMIC_CACHE_NAME cache for future fetch requests
+--else if internet connection is unavailable then a fallback page (Offline.html) is shown to the user only if the requested page is of *.html format
+*/
+self.addEventListener("fetch", (event) => {
+  // console.log("Service worker if fetching", event);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((cacheResponse) => {
+        return (
+          cacheResponse ||
+          fetch(event.request).then((fetchResponse) => {
+            return caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
+              cache.put(event.request.url, fetchResponse.clone());
+              return fetchResponse;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (event.request.url.indexOf(".html") > -1) {
+          return caches.match("/offline.html");
+        }
+      })
+  );
+});
--- src/App.js
+++ src/App.js
@@ -1,117 +1,240 @@
 import { useQuery, gql, useMutation } from "@apollo/client";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const GET_RECORDS = gql`
-    query Records {
-        queryRecord {
-            id
-            date
-            symptom
-        }
+  query Records {
+    queryRecord {
+      id
+      date
+      symptom
     }
+  }
 `;
 
 const ADD_RECORD = gql`
-    mutation MyMutation($date: String!, $symptoms: [Symptom!]!) {
-        addRecord(input: { date: $date, symptom: $symptoms }) {
-            record {
-                id
-                date
-                symptom
-            }
-        }
+  mutation MyMutation($date: String!, $symptoms: [Symptom!]!) {
+    addRecord(input: { date: $date, symptom: $symptoms }) {
+      record {
+        id
+        date
+        symptom
+      }
     }
+  }
 `;
 
-function AddData() {
-    const [addDataFunction, { data, loading, error }] = useMutation(ADD_RECORD);
-    const [props, setProps] = useState([
-        ["itching", 0],
-        ["watery_eyes", 0],
-        ["runny_nose", 0],
-        ["sneezing", 0],
-        ["nasal_inflammation", 0],
-        ["swollen_throat", 0],
-    ]);
-    const toggle = (idx) => {
-        let temp = [...props];
-        temp[idx][1] = 1 - temp[idx][1];
-        setProps(temp);
-    };
-    const addRecord = () => {
-        let date = new Date().toLocaleString();
-        let symptoms = props.filter((prop) => prop[1]).map((prop) => prop[0]);
-        console.log(date, symptoms);
-        addDataFunction({ variables: { date, symptoms } });
-        if (error) console.log(error);
-        if (data) {
-            console.log(data);
-        }
-    };
-    return (
-        <div className="form-flex">
-            <h2>Select Symptoms</h2>
-            <div className="btn-flex">
-                {props.map((prop, idx) => (
-                    <button
-                        key={idx}
-                        className={"btn" + (prop[1] ? " selected" : "")}
-                        onClick={() => toggle(idx)}
-                    >
-                        {prop[0].replaceAll("_", " ")}
-                    </button>
-                ))}
-            </div>
-            <button onClick={() => addRecord()} className="btn btn-process">
-                {loading ? "Recording..." : "Record Dose"}
-            </button>
-            {error && <p className="error">{error.message}</p>}
-        </div>
-    );
+const DELETE_RECORD = gql`
+  mutation MyMutation($id: [ID!]!) {
+    deleteRecord(filter: { id: $id }) {
+      record {
+        id
+        date
+        symptom
+        __typename
+      }
+    }
+  }
+`;
+
+function AddData({ data, setData }) {
+  const [addDataFunction, { data: mutatedData, loading, error }] =
+    useMutation(ADD_RECORD);
+  const [props, setProps] = useState([
+    ["itching", 0],
+    ["watery_eyes", 0],
+    ["runny_nose", 0],
+    ["sneezing", 0],
+    ["nasal_inflammation", 0],
+    ["swollen_throat", 0],
+  ]);
+  useEffect(() => {
+    if (mutatedData?.addRecord?.record.length) {
+      console.log(data);
+      let newData = [...data, mutatedData.addRecord.record[0]];
+      setData(newData);
+    }
+  }, [mutatedData]);
+  const toggle = (idx) => {
+    let temp = [...props];
+    temp[idx][1] = 1 - temp[idx][1];
+    setProps(temp);
+  };
+  const addRecord = () => {
+    function getDifferenceInHours(dateString1, dateString2) {
+      const date1 = new Date(dateString1);
+      const date2 = new Date(dateString2);
+
+      const differenceInMilliseconds = date2.getTime() - date1.getTime();
+      const differenceInHours = differenceInMilliseconds / 3600000;
+
+      return differenceInHours;
+    }
+
+    let date = new Date().toLocaleString();
+    if (
+      data.length &&
+      getDifferenceInHours(data[data.length - 1].date, date) < 6
+    ) {
+      alert(
+        "You cannot take medicine in less than 6 hours of your previous dose."
+      );
+      return;
+    }
+    let symptoms = props.filter((prop) => prop[1]).map((prop) => prop[0]);
+    console.log(date, symptoms);
+    addDataFunction({ variables: { date, symptoms } });
+  };
+  return (
+    <div className="form-flex">
+      <h2>Select Symptoms</h2>
+      <div className="btn-flex">
+        {props.map((prop, idx) => (
+          <button
+            key={idx}
+            className={"btn" + (prop[1] ? " selected" : "")}
+            onClick={() => toggle(idx)}
+          >
+            {prop[0].replaceAll("_", " ")}
+          </button>
+        ))}
+      </div>
+      <button onClick={() => addRecord()} className="btn btn-process">
+        {loading ? "Recording..." : "Record Dose"}
+      </button>
+      {error && <p className="error">{error.message}</p>}
+    </div>
+  );
 }
 
-function DisplayRecords() {
-    const { loading, error, data } = useQuery(GET_RECORDS);
+function DeleteSelected({ record, setRecord, selectedRecord, setSelected }) {
+  const [deleteRecordFunction, { data: deletedRecord, loading, error }] =
+    useMutation(DELETE_RECORD);
+  useEffect(() => {
+    if (deletedRecord?.deleteRecord?.record.length) {
+      let newArr = selectedRecord.filter((r) => {
+        let bool = true;
+        deletedRecord.deleteRecord.record.map((y) => {
+          if (y.id === r) {
+            bool = false;
+          }
+        });
+        return bool;
+      });
+      setSelected(newArr);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
+      newArr = record.filter((x) => {
+        let bool = true;
+        deletedRecord.deleteRecord.record.map((y) => {
+          if (y.id === x.id) {
+            bool = false;
+          }
+          return true;
+        });
+        return bool;
+      });
+      setRecord(newArr);
+    }
+  }, [deletedRecord]);
+  const deleteRecord = () => {
+    deleteRecordFunction({ variables: { id: selectedRecord } });
+  };
+  return (
+    <button onClick={() => deleteRecord()} className="btn btn-delete">
+      Delete Records
+    </button>
+  );
+}
+function DisplayRecords({
+  loading,
+  error,
+  record,
+  setRecord,
+  select,
+  setSelected,
+}) {
+  const toggleRecord = (e, id) => {
+    if (e.target.checked) {
+      setSelected([...select, id]);
+    } else {
+      setSelected(select.filter((x) => x !== id));
+    }
+  };
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  if (record?.length) {
     return (
-        <div>
-            <table id="records">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Date</th>
-                        <th>Symptoms</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.queryRecord.map((data) => (
-                        <tr key={data.id}>
-                            <td>{data.id}</td>
-                            <td>{data.date}</td>
-                            <td>
-                                <ul>
-                                    {data.symptom.map((s) => (
-                                        <li key={s}>{s.replaceAll("_", " ")}</li>
-                                    ))}
-                                </ul>
-                            </td>
-                        </tr>
+      <div id="table-wrapper">
+        <table id="records">
+          <thead>
+            <tr>
+              <th>
+                {select.length ? (
+                  <DeleteSelected
+                    record={record}
+                    setRecord={setRecord}
+                    selectedRecord={select}
+                    setSelected={setSelected}
+                  />
+                ) : (
+                  "Select"
+                )}
+              </th>
+              <th>Sr. No.</th>
+              {/* <th>ID</th> */}
+              <th>Date</th>
+              <th>Symptoms</th>
+            </tr>
+          </thead>
+          <tbody>
+            {record.map((record, idx) => (
+              <tr key={record.id}>
+                <td>
+                  <input
+                    onChange={(e) => toggleRecord(e, record.id)}
+                    type="checkbox"
+                  ></input>
+                </td>
+                <td>{idx + 1}</td>
+                {/* <td>{record.id}</td> */}
+                <td>{record.date}</td>
+                <td>
+                  <ul>
+                    {record.symptom.map((s) => (
+                      <li key={s}>{s.replaceAll("_", " ")}</li>
                     ))}
-                </tbody>
-            </table>
-        </div>
+                  </ul>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
+  }
+  return <h1>Start Recording Data</h1>;
 }
 
 export default function App() {
-    return (
-        <div>
-            <h1>TrackMyMeds</h1>
-            <AddData />
-            <DisplayRecords />
-        </div>
-    );
+  const { loading, error, data } = useQuery(GET_RECORDS);
+  const [dataRecord, setData] = useState(data);
+  const [selectedRecord, setSelected] = useState([]);
+
+  useEffect(() => {
+    setData(data?.queryRecord);
+  }, [data]);
+  return (
+    <div>
+      <h1>TrackMyMeds</h1>
+      <AddData data={dataRecord} setData={setData} />
+      <DisplayRecords
+        loading={loading}
+        error={error}
+        record={dataRecord}
+        setRecord={setData}
+        select={selectedRecord}
+        setSelected={setSelected}
+      />
+    </div>
+  );
 }
